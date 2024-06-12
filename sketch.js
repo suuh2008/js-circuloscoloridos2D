c7e78dbb2bddf1599b6dba83f0478784de2b0148
suuh2008 <suzanareboucas9@gmail.com>
2024-06-12 09:51:20 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let cor
+let posicaoHorizontal//x
+letposicaoVertical/y
+
+funcion setup() {
+   createCanvas(400, 400);
+  background("white");
+  cor = color(random(0. 255), random(0, 255) random (o, 255));
+  pocaoHorizontal = 200;
+  posicaoVerdical = 200;
+}
+
+function draw(){
+  fill(cor);
+  circle9(posicaoHorizontal, posicaoVertical, 50); 
+    background("pink");
+
+}
+
+function draw() {
+fill('blue')  
+circle(200,200,50)
+}
